bd1e01f5a94a2778a331621a8f0ed2158dd28b32
qnnn <65326092+qnnn@users.noreply.github.com>
2023-10-01 23:35:21 +0800

    优化服务注册性能 (#9)

--- store/mysql/scripts/instance_v2_append.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `polaris_server`.`instance`
-ADD COLUMN `health_check_type` tinyint NULL COMMENT 'Instance health check type' AFTER `flag`,
-ADD COLUMN `health_check_ttl` int NULL COMMENT 'TTL time jumping' AFTER `health_check_type`,
-ADD COLUMN `metadata` text NULL COMMENT 'Instance Label' AFTER `health_check_ttl`;
